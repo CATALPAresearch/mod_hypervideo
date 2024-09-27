2aa37963bc441241e121df31bea66921cd5f38b4
nise <niels.seidel@nise81.com>
2024-09-27 09:34:41 +0200

    improved code readbaility

--- settings.php
+++ settings.php
@@ -1,14 +1,14 @@
 <?php
-    
-    defined('MOODLE_INTERNAL') || die();
-    
-    $settings->add(
-        new admin_setting_configtext(
-            'hypervideo/someconfig', 
-            get_string('confentry', 'hypervideo'),
-            get_string('confdescription', 'hypervideo'), 
-            get_string('confvalue', 'hypervideo'), 
-            PARAM_RAW, 
-            40
-        )
-    );
+
+defined('MOODLE_INTERNAL') || die();
+
+$settings->add(
+    new admin_setting_configtext(
+        'hypervideo/someconfig',
+        get_string('confentry', 'hypervideo'),
+        get_string('confdescription', 'hypervideo'),
+        get_string('confvalue', 'hypervideo'),
+        PARAM_RAW,
+        40
+    )
+);
--- view.php
+++ view.php
@@ -4,67 +4,62 @@ require('../../config.php');
 require_once('lib.php');
 
 
-$id = optional_param('id', 0, PARAM_INT); // Course Module ID
-$g  = optional_param('g', 0, PARAM_INT);  // hypervideo ID
 
-if (!empty($id)) {
-    if (! $cm = get_coursemodule_from_id('hypervideo', $id)) {
-        print_error('invalidcoursemodule');
+$course_id = optional_param('id', 0, PARAM_INT); // Course Module ID
+$hypervideo_id  = optional_param('g', 0, PARAM_INT);  // hypervideo ID
+
+if (!empty($course_id)) {
+    if (! $cm = get_coursemodule_from_id('hypervideo', $course_id)) {
+        print_error('invalidcoursemodule', 'hypervideo');
     }
-    if (! $course = $DB->get_record("course", array("id"=>$cm->course))) {
-        print_error('coursemisconf');
+    if (! $course = $DB->get_record("course", array("id" => $cm->course))) {
+        print_error('coursemisconf', 'hypervideo');
     }
-    if (! $hypervideo = $DB->get_record("hypervideo", array("id"=>$cm->instance))) {
+    if (! $hypervideo = $DB->get_record("hypervideo", array("id" => $cm->instance))) {
         print_error('invalidid', 'hypervideo');
     }
-
-} else if (!empty($g)) {
-    if (! $hypervideo = $DB->get_record("hypervideo", array("id"=>$g))) {
+} else if (!empty($hypervideo_id)) {
+    if (! $hypervideo = $DB->get_record("hypervideo", array("id" => $hypervideo_id))) {
         print_error('invalidid', 'hypervideo');
     }
-    if (! $course = $DB->get_record("course", array("id"=>$hypervideo->course))) {
-        print_error('invalidcourseid');
+    if (! $course = $DB->get_record("course", array("id" => $hypervideo->course))) {
+        print_error('invalidcourseid', 'hypervideo');
     }
     if (!$cm = get_coursemodule_from_instance("hypervideo", $hypervideo->id, $course->id)) {
-        print_error('invalidcoursemodule');
+        print_error('invalidcoursemodule', 'hypervideo');
     }
-    $id = $cm->id;
+    $course_id = $cm->id;
 } else {
     print_error('invalidid', 'hypervideo');
 }
+
 require_login($course, true, $cm);
 
-
 if ($cm) {
-    $PAGE->set_cm($cm, $course); 
+    $PAGE->set_cm($cm, $course);
 } else {
     $PAGE->set_course($course);
 }
 $PAGE->set_pagelayout('base');
-$PAGE->set_title($course->shortname.': '.$hypervideo->name);
+$PAGE->set_title($course->shortname . ': ' . $hypervideo->name);
 $PAGE->set_heading($hypervideo->name);
 $PAGE->set_url('/mod/hypervideo/view.php', array('id' => $cm->id));
 
 echo $OUTPUT->header();
+echo html_writer::div('', 'app', ['id' => 'app']);
 
-//echo ''.$hypervideo->id;
-//echo var_dump($hypervideo);
-echo <<<'EOT'
-<div id="app"></div>
-EOT;
 $PAGE->requires->js_call_amd('mod_hypervideo/app-lazy', 'init', [
     'coursemoduleid' => $cm->id,
-    'contextid' => 0,//$cm->context->id,
+    'contextid' => $cm->context->id,
     'courseid' => $course->id,
     'hypervideoid' => $hypervideo->id,
     'fullPluginName' => 'mod_hypervideo',
     'url' => $hypervideo->url,
-    'title' => 'Video: '. $hypervideo->name
+    'title' => 'Video: ' . $hypervideo->name
 ]);
+
 echo $OUTPUT->footer($course);
 
 //$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 //var_dump(json_decode($json));
 //var_dump(unserialize($json));
-
-
